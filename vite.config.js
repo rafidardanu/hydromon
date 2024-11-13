eda16aa4b4194625fe66d7aa6d28c0c203aeffbc
rafidardanu <rescuephones2016@gmail.com>
2024-11-13 23:36:17 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ["mqtt"],
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["mqtt"],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
   },
 });
